444a3d9a5d672020a53e96a6d4cf1f02634ecb16
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-15 11:37:35 +0200

    Creating the spotlight

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -6,6 +6,18 @@ namespace Hangman
 {
     public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        #region UI Properties
+        public string Spotlight
+        {
+            get => spotlight; 
+            set
+            {
+                spotlight = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
         #region Fields
         List<string> words = new List<string>()
      {
@@ -24,12 +36,17 @@ namespace Hangman
         "snippets"
      };
         string answer = "";
+        private string spotlight;
+        List<char> guessed =
+            new List<char>();
         #endregion
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
             PickWord();
+            CalculateWord(answer, guessed);
         }
 
         #region Game Engine
@@ -38,6 +55,15 @@ namespace Hangman
             answer = words[new Random().Next(0, words.Count)];
             Debug.WriteLine(answer);
         }
+
+        private void CalculateWord(string answer, List<char> guessed) 
+        { 
+            var temp = 
+                answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_'))
+                .ToArray();
+            Spotlight = string.Join(' ', temp);
+        }
+
         #endregion
     }
 }

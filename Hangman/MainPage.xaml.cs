9ac1fb1e052c078c126438b5941726dc401dbfbe
nic00la1 <nic00la.kaleta@gmail.com>
2023-10-15 10:15:56 +0200

    Add project files.

---
+++ Hangman/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Hangman
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
